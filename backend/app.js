9b1c74352e716deffcb2f2c5f0c17f1621b1b336
momynul-islam <islammomynul.cod@gmail.com>
2025-11-14 12:42:24 +0600

    cors and socketio config update

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "*",
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
